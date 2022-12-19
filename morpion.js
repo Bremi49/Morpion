f322f2d987cc5d4c4b029491dcd38edf7fbd55fb
Bremi49 <remibouilly@hotmail.fr>
2022-12-19 09:40:11 +0000

    morpion2

--- morpion.js
+++ morpion.js
@@ -1,19 +1,31 @@
-//Les case vides sert a jouer
-let game = ["","",""]
-//premier player
-const player=0
-//deuxieme player
-const player1=0
 //possibilites de victoires
-const victoire = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-    ]
+const victory = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+const starCircle = () => {
+    //premier player
+    const player = 0
+    //deuxieme player
+    const player1 = 1
+    
+    const caseContent = document.getElementsByClassName("row")
+    
+}
+//Intéragir pour que chaque joeur joue tour par tour
+const ticTurn = () => {
+    //Les case vides servent a jouer
+    let game = ["", "", ""]
+}
 
 
+document.addEventListener("DOMContentLoaded", () => {
+
+})
