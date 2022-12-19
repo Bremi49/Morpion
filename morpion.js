d28ef05ab5c80ccd3c011fda4d502b8fae2bc51b
Bremi49 <remibouilly@hotmail.fr>
2022-12-19 09:08:33 +0000

    morpion

---
+++ morpion.js
@@ -0,0 +1,19 @@
+//Les case vides sert a jouer
+let game = ["","",""]
+//premier player
+const player=0
+//deuxieme player
+const player1=0
+//possibilites de victoires
+const victoire = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+    ]
+
+
